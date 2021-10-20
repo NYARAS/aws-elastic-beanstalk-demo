8ffb638c2a2808fe86ebbb1c016404ab60a88a22
Otieno Calvine <nyarangaotieno@gmail.com>
2021-10-21 01:09:47 +0300

    fix: rename application files
    Using application.py as the filename and providing a callable application object (the Flask object, in this case) allows AWS Elastic Beanstalk to easily find your application' code
    
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- test_application.py
+++ test_application.py
@@ -1,5 +1,5 @@
 """Test suite for app."""
-from app import application
+from application import application
 with application.test_client() as c:
     response = c.get('/')
     assert response.data == b'Hello World!'
